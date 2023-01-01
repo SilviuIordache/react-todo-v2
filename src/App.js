137a46b9c7350f13118414c256d52c40270578e6
SilviuIordache <silviu.iordache92@gmail.com>
2023-01-01 19:28:18 +0200

    Switch to the example from React Docs

--- src/App.js
+++ src/App.js
@@ -1,56 +1,13 @@
-import { useReducer } from 'react';
-import AddNewItem from './components/AddNewItem';
-import ItemList from './components/ItemList';
+import AddTask from './components/AddTask';
+import TaskList from './components/TaskList.js';
+import { TasksProvider } from './components/TasksContext.js';
 
-export const ACTIONS = {
-  ADD_TODO: 'add-todo',
-  TOGGLE_TODO: 'toggle-todo',
-  DELETE_TODO: 'delete-todo',
-  EDIT_TODO: 'edit-todo',
-};
-
-function toDoReducer(todos, action) {
-  switch (action.type) {
-    case ACTIONS.ADD_TODO:
-      return [...todos, newToDo(action.payload.name)];
-    case ACTIONS.TOGGLE_TODO:
-      return todos.map((todo) => {
-        if (todo.id === action.payload.id) {
-          return { ...todo, complete: !todo.complete };
-        }
-        return todo;
-      });
-    case ACTIONS.DELETE_TODO:
-      return todos.filter((todo) => todo.id !== action.payload.id);
-    case ACTIONS.EDIT_TODO:
-      return todos.map((todo) => {
-        if (todo.id === action.payload.id) {
-          return { ...todo, name: action.payload.name };
-        }
-        return todo;
-      });
-    default:
-      return todos;
-  }
-}
-
-function newToDo(name) {
-  return { id: Date.now(), name, complete: false };
-}
-
-function App() {
-  const [todos, dispatch] = useReducer(toDoReducer, [
-    { id: 1, name: 'clean', complete: false },
-    { id: 2, name: 'wash', complete: false },
-    { id: 3, name: 'rest', complete: false },
-  ]);
-
+export default function TaskApp() {
   return (
-    <div className="App">
-      <AddNewItem dispatch={dispatch} />
-      <ItemList todos={todos} dispatch={dispatch} />
-    </div>
+    <TasksProvider>
+      <h1>Day off in Kyoto</h1>
+      <AddTask />
+      <TaskList />
+    </TasksProvider>
   );
 }
-
-export default App;
--- src/components/ItemList.js
+++
@@ -1,9 +0,0 @@
-import ToDoItem from './ToDoItem';
-
-export default function ItemList({ todos, dispatch }) {
-  // TODO:
-  // either merge this ItemList with ToDoItem
-  // or change to context to avoid prop drilling
-  const items = todos.map((todo) => <ToDoItem key={todo.id} todo={todo} dispatch={dispatch}/>);
-  return items;
-}
